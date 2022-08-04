7e3bd1eb8a1561857108f71e017954b47bfdef34
Radu Ciocan <ciocan@gmail.com>
2022-08-04 16:51:44 +0100

    add header and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,26 @@
 export default function Footer() {
-  return <footer className="bg-pink-100">footer</footer>;
+  return (
+    <footer className="flex justify-between p-2 text-xs">
+      <div>&copy; 2022 All rights reserved</div>
+      <div className="flex gap-2">
+        <a
+          href="https://docs.burrow.cash"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline underline-offset-4"
+        >
+          Terms of Service
+        </a>
+        <span>|</span>
+        <a
+          href="https://docs.burrow.cash"
+          target="_blank"
+          rel="noreferrer"
+          className="hover:underline underline-offset-4"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,27 @@
+import { ReactComponent as LogoSvg } from "~/assets/logo.svg";
+
 export default function Header() {
-  return <header className="bg-pink-100">header</header>;
+  return (
+    <header className="text-sm flex gap-6 p-4 bg-[#000741] text-white h-[60px] items-center">
+      <span className="pb-1">
+        <LogoSvg />
+      </span>
+      <a
+        href="https://docs.burrow.cash/product-docs/how-burrow-works/governance"
+        target="_blank"
+        rel="noreferrer"
+        className="hover:border-b-2 pb-1 hover:border-[#47c285]"
+      >
+        Governance
+      </a>
+      <a
+        href="https://docs.burrow.cash"
+        target="_blank"
+        rel="noreferrer"
+        className="hover:border-b-2 pb-1 hover:border-[#47c285]"
+      >
+        Docs
+      </a>
+    </header>
+  );
 }
