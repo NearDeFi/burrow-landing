d25d6b5d43231db3acbb70a8d43f58cc2baabcf8
Radu Ciocan <ciocan@gmail.com>
2022-08-04 14:46:17 +0100

    add eslint, prettier & husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 function App() {
   return (
     <div className="flex h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">
-        Burrow landing page
-      </h1>
+      <h1 className="text-2xl font-bold">Burrow landing page</h1>
     </div>
   );
 }
